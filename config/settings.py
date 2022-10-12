5e7dd3cb3108df7d93efb8883a3b0622cc45f43f
wodnrP <wogajang22@naver.com>
2022-10-12 11:27:57 +0900

    cors 수정

--- config/settings.py
+++ config/settings.py
@@ -98,9 +98,7 @@ DATABASES = {
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = (
-    ['https://localhost:3000', 'https://localhost:8000', 'https://hobbydiscovery.herokuapp.com']
-)
+CORS_ORIGIN_WHITELIST = True
 
 # [
 #     'https://hobbydiscovery.herokuapp.com',
