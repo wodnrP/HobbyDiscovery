43d477aaeaf2ef68656be567b03f4a61c3628194
wodnrP <wogajang22@naver.com>
2022-10-16 19:11:48 +0900

    static 수정

--- config/settings.py
+++ config/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
     ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 # Media Files
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
