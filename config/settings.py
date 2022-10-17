cd96f52bc299c63c4aad517be55580376efa300e
wodnrP <wogajang22@naver.com>
2022-10-17 15:45:18 +0900

    image 5차 수정

--- config/settings.py
+++ config/settings.py
@@ -143,6 +143,7 @@ STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
     ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 # Media Files
 MEDIA_URL ='/media/'
