eb741e4f46b7655507e895300c82d9fe7eb567c4
wodnrP <wogajang22@naver.com>
2022-10-17 15:18:57 +0900

    image 4차 수정

--- config/settings.py
+++ config/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 # Media Files
-MEDIA_URL ='media/'
+MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
